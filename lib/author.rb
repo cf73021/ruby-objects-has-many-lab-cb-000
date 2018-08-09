2d932dac1c4a2c09abc443cbe26efa5cdeaca24f
Chris Franklin <cf73021@gmail.com>
2018-08-09 20:42:42 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,29 +1,8 @@
 class Artist
   attr_accessor :name
-  @@songs = []
 
   def initialize(name)
     @name = name
-    @songs = []
-  end
-
-  def add_song(song)
-    @songs << song
-    song.artist = self
-    @@songs << song
-  end
-
-  def songs
-    @songs
-  end
-
-  def add_song_by_name(name)
-    song =  Song.new(name)
-    self.add_song(song)
-  end
-
-  def self.song_count
-    @@songs.count
   end
 
 end
