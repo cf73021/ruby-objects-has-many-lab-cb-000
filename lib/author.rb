1f1dc979606d8a51dc4df7019b389d8ff9e9b4a9
Chris Franklin <cf73021@gmail.com>
2018-08-09 20:46:28 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,12 +1,16 @@
 class Author
   attr_accessor :name, :posts
-  @@posts = []
+  @@posts = [
+
+  ]
   def initialize(name)
     @name = name
     @posts = []
   end
 
-  def add_post
-
+  def add_post(post)
+    post.author = self
+    @posts << post
+    @@posts << post
 
 end
